ab6506f4adb3f4cfdc76e2b4b4e9b2e2379f2651
vinodell <max.khamitov@mail.ru>
2021-04-21 23:59:28 +0300

    add curves and data

--- client/components/graph.js
+++ client/components/graph.js
@@ -1,35 +1,80 @@
-import React from 'react'
-// import React, { useState, useEffect } from 'react'
-// import { scaleLinear, scaleTime } from 'd3-scale'
+import React, { useState, useEffect } from 'react'
+import { scaleLinear, scaleTime } from 'd3-scale'
+import {
+  min, max, select, axisLeft, axisBottom, curveCardinal, line
+} from 'd3'
 
-// import data from '../../data.json'
+import data from '../../data.json'
 
-// const DEFAULT_HEIGHT = 1200
-// const DEFAULT_WIDTH = 400
-// const X_MARGIN = 20
-// const Y_MARGIN = 50
+const DEFAULT_HEIGHT = 400
+const DEFAULT_WIDTH = 1000
+const X_MARGIN = 20
+const Y_MARGIN = 50
 
-// const drawLine = ({ width, height }) => {
-//   const getNewAxis = (cx) => select('#chart').append('g').attr('class', cx)
+const info = data
+  .map((it) => ({
+    value: +it.value,
+    year: +it.year
+  }))
+  .filter((it) => !Number.isNaN(it.value))
 
-//   const scaleX = scaleLinear().domain().range()
+const drawLine = ({ width, height }) => {
+  const getNewAxis = (cx) => select('#chart').append('g').attr('class', cx)
 
-//   const scaleY = scaleTime().domain().range()
-// }
+  const scaleX = scaleTime()
+    .domain([min(info.map((it) => it.year)), max(info.map((it) => it.year))])
+    .range([2 * X_MARGIN, width - X_MARGIN * 10])
+
+  const scaleY = scaleLinear()
+    .domain([min(info.map((it) => it.value)), max(info.map((it) => it.value))])
+    .range([height - Y_MARGIN * 2, Y_MARGIN])
+
+  const Yax = select('.y-axis')
+  const Xax = select('.x-axis')
+
+  const Yaxis = axisLeft().scale(scaleY)
+  const Xaxis = axisBottom().scale(scaleX);
+
+  (Yax.empty() ? getNewAxis('y-axis') : Yax)
+    .transition()
+    .attr('transform', `translate(${2 * X_MARGIN}, ${Y_MARGIN})`)
+    .call(Yaxis);
+
+  (Xax.empty() ? getNewAxis('x-axis') : Xax)
+    .transition()
+    .attr('transform', `translate(${0}, ${height - Y_MARGIN})`)
+    .call(Xaxis)
+
+  const chartLine = line()
+    .curve(curveCardinal)
+    .x((d) => scaleX(d.year))
+    .y((d) => scaleY(d.value))
+
+  const chartPath = select('.path');
+  (chartPath.empty() ? select('#chart').append('path').attr('class', 'path') : chartPath)
+    .datum(info)
+    .attr('stroke', 'grey')
+    .attr('stroke-width', '2')
+    .attr('fill', 'none')
+    .transition()
+    .attr('d', chartLine)
+}
 
 const Graph = () => {
-  // const [width] = useState(DEFAULT_HEIGHT)
-  // const [height] = useState(DEFAULT_WIDTH)
-  // useEffect(() => {
-  //   drawLine({ width, height })
-  // }, [width, height])
+  const [width] = useState(DEFAULT_WIDTH)
+  const [height] = useState(DEFAULT_HEIGHT)
+
+  useEffect(() => {
+    drawLine({ width, height })
+  }, [width, height])
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is Graph dude
+      <div className="min-w-screen min-h-screen bg-gray-900 flex flex-wrap content-around justify-center px-5 py-5">
+        <div className="bg-indigo-600 text-white rounded shadow-xl py-5 px-5 w-full lg:w-10/12 xl:w-3/4">
+          <div className="flex items-end">
+            <svg width={width} height={height} id="chart" />
+          </div>
         </div>
-        {/* <svg width={width} height={height} id="chart" /> */}
       </div>
     </div>
   )
