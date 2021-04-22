40250fa1b2a8c97a5571f7a30139b4aba7679522
vinodell <max.khamitov@mail.ru>
2021-04-22 16:12:26 +0300

    find way to get checkbox info from data.json

--- client/components/checkbox.js
+++ client/components/checkbox.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import info from '../../data.json'
+
+const getLevel1Info = info.reduce((acc, rec) => {
+  return [...acc, rec.level_1]
+}, [])
+const [...checkBoxInfo] = new Set(getLevel1Info)
+
+const data = info.reduce((acc, rec) => {
+  return [...acc, { [rec.level_1]: false }]
+}, [])
+
+const initialState = {
+  ...data
+}
+
+console.log(initialState)
 
 const Checkbox = () => {
-  return <div>this is checkbox</div>
+  // const [store, dispatch] = useReducer(reducerFunc, initialState)
+  const [isChecked, setChecked] = useState(true)
+  const onChange = (e) => {
+    setChecked(e.target.checked)
+  }
+
+  return (
+    <div>
+      <legend>Choose your data for graph</legend>
+      {checkBoxInfo.map((it, index) => {
+        return (
+          <div key={index}>
+            {it}
+            <input type="checkbox" id={index} checked={isChecked} onChange={onChange} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Checkbox
+
+// level_1
+// Total Citizen
+// Total Male Citizens
+// Total Female Citizens
+// Total Male Citizens
+// Total Female Citizens
+// Total Malays
+// Total Male Malays
+// Total Female Malays
+// Total Chinese
+// Total Male Chinese
+// Total Female Chinese
+// Total Indians
+// Total Male Indians
+// Total Female Indians
+// Other Ethnic Groups (Total)
+// Other Ethnic Groups (Males)
+// Other Ethnic Groups (Females)
+
+// level_2
+// 0  -  4 Years
+// 5  -  9 Years
+// 10 - 14 Years
+// 15 - 19 Years
+// 20 - 24 Years
+// 25 - 29 Years
+// 30 - 34 Years
+// 35 - 39 Years
+// 40 - 44 Years
+// 45 - 49 Years
+// 50 - 54 Years
+// 55 - 59 Years
+// 60 - 64 Years
+// 65 - 69 Years
+// 70 - 74 Years
+// 75 - 79 Years
+// 80 - 84 Years
+// 65 Years & Over
+// 70 Years & Over
+// 75 Years & Over
+// 80 Years & Over
+// 85 Years & Over
